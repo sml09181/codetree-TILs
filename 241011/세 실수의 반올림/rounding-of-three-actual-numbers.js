1988bd323084eb479eb10c8510c12d45d55e9b2a
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-11 12:24:07 +0900

    Title: 세 실수의 반올림, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

--- 241011/세 실수의 반올림/rounding-of-three-actual-numbers.js
+++ 241011/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -3,4 +3,4 @@ let input = fs.readFileSync(0).toString().trim().split('\n');
 let [a, b, c] = input;
 console.log(`${(+a).toFixed(3)}
 ${(+b).toFixed(3)}
-${(+b).toFixed(3)}`);
+${(+c).toFixed(3)}`);

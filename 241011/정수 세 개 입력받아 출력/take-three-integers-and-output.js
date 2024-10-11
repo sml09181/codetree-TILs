36a24ec08df7f1daa5dab83eca45cfda5f3b903d
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-11 12:25:43 +0900

    Title: 정수 세 개 입력받아 출력, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 241011/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 241011/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n');
-let [a, b, c] = input;
+let [temp, c] = input;
+let [a, b] = temp.split(' ');
 console.log(`${+a} ${+b} ${+c}`);

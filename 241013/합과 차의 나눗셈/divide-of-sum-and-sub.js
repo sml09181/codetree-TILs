271564a18ddf52318403ab4ae4d415cd9114196a
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-13 10:59:35 +0900

    Title: 합과 차의 나눗셈, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241013/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+console.log(parseFloat((a+b)/(a-b)).toFixed(2));

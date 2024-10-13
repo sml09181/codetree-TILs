0469a45357f7583d674c5770a8d9e04c499a387a
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-13 11:12:11 +0900

    Title: 합과 평균, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 241013/합과 평균/sum-and-avg.js
+++ 241013/합과 평균/sum-and-avg.js
@@ -2,5 +2,4 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
-let avg = (a+b)/2;
-console.log("%d %d", a+b, avg.toFixed(1));
+console.log(a+b, ((a+b)/2).toFixed(1));

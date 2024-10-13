5ff82ffc62ab0fdf1de61c478bdee23d381f9281
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-13 10:54:01 +0900

    Title: 몫과 나머지, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 241013/몫과 나머지/quotient-remainder.js
+++ 241013/몫과 나머지/quotient-remainder.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
-console.log("%d...%d", parseInt(a/b). a%b);
+console.log("%d...%d", parseInt(a/b), a%b);

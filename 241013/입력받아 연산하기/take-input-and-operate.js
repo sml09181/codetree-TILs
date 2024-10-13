f155b07ffeb23f24b8ed40fab7a73d32ccc24fbc
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-10-13 10:56:22 +0900

    Title: 입력받아 연산하기, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 241013/입력받아 연산하기/take-input-and-operate.js
+++ 241013/입력받아 연산하기/take-input-and-operate.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(' ');
+let input = fs.readFileSync(0).toString().trim().split('\n');
 let a = Number(input[0])+87;
 let b = Number(input[1])%10;
 console.log("%d\n%d", a, b);
